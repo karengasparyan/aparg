809bcd3435ee73a34780a891cc271ed7a9170587
Karen Gasparyan <karengasparyan.a@gmai.com>
2022-06-29 12:59:59 +0400

    fixed desagn missing

--- src/Components/Story/index.js
+++ src/Components/Story/index.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import './style.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronDown, faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import {faChevronDown, faChevronUp, faThumbsUp, faThumbsDown, faBookmark} from "@fortawesome/free-solid-svg-icons";
 
 const Story = ({id, imageUrls, expanded_url, title, domain_cached_logo_url, domain_host, description, score}) => {
-  const [show, setShow] = useState(false);
+  const [height, setHeight] = useState(100);
+  const [showBookmarks, setShowBookmarks] = useState(false);
   const color = score > 40 ? "#4eb495" : "#ffb300";
 
   const openInNewTab = url => {
@@ -12,26 +13,44 @@ const Story = ({id, imageUrls, expanded_url, title, domain_cached_logo_url, doma
   };
 
   const showDescription = () => {
-    setShow(!show);
+    setHeight(height => {
+      return height > 100 ? 100 : 200
+    })
   };
 
-  const handleError = (ev) => {
-    return ev.target.src = "/images/user-286.png"
-  }
+  const handleShowBookmarks = () => {
+
+    setShowBookmarks(showBookmarks => !showBookmarks)
+  };
 
   return (
-    <div className="mask" key={id}>
-      <img className="image" src={imageUrls && imageUrls[0]} alt="image"/>
-      <div>
-        <p onClick={() => openInNewTab(expanded_url)} className="title">{title}</p>
-        <div className="infoContainer">
-          <img onError={handleError} src={domain_cached_logo_url} alt="image"/>
-          <p>{domain_host}</p>
+    <div className="maskContainer">
+      <div className="mask" style={{height: height}} key={id}>
+        <img className="image" src={imageUrls ? imageUrls[0] : "/images/notfound.png"} alt="image"/>
+        <div>
+          <p onClick={() => openInNewTab(expanded_url)} className="title">{title}</p>
+          <div className="infoContainer">
+            <img src={domain_cached_logo_url} alt="image"/>
+            <p>{domain_host}</p>
+          </div>
+          <div style={{border: `solid 1px ${color}`}} className="percent">{score}%</div>
+          <FontAwesomeIcon onClick={showDescription} icon={height > 100 ? faChevronUp : faChevronDown}
+                           className="chevronDown"/>
+          {height > 100 ? <div>
+            <span className="description">{description}</span>
+            <div className="bookmarkContainer">
+              {showBookmarks && <span>
+              <span className="bookmarkText">Like</span><FontAwesomeIcon icon={faThumbsUp} className="thumbs"/>
+              <span className="bookmarkText">Dislike</span><FontAwesomeIcon icon={faThumbsDown} className="thumbs"/>
+            </span>}
+              <span onClick={handleShowBookmarks}>
+              <span className="bookmarkText">Bookmark</span><FontAwesomeIcon icon={faBookmark} className="thumbs"/>
+            </span>
+            </div>
+          </div> : null}
         </div>
-        <div style={{border: `solid 1px ${color}`}} className="percent">{score}%</div>
-        <FontAwesomeIcon onClick={showDescription} icon={show ? faChevronUp : faChevronDown} className="chevronDown"/>
-        {show && <p>{description}</p>}
       </div>
+
     </div>
   );
 };
